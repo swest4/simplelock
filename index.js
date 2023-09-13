41b6a1cd2df602ab9f0af5f9ed115e816a4472dd
Shane West <shane.west@regalcinemas.com>
2023-09-13 16:36:09 -0400

    Initial commit for door unlock util.

---
+++ index.js
@@ -0,0 +1,35 @@
+import { exec } from 'child_process';
+import dotenv from 'dotenv';
+import Seam from 'seamapi';
+
+dotenv.config({ path: '.env' });
+
+const seam = new Seam();
+const lockId = process.env.LOCK;
+console.log(lockId);
+
+const unlock = async () => {
+  const attempt = await seam.locks.unlockDoor(lockId);
+  console.log(attempt);
+  if (attempt.action_attempt.status === 'success') {
+    exec('termux-toast "Door unlocked"');
+  }
+  process.exit();
+};
+
+const lock = async () => {
+  const attempt = await seam.locks.lockDoor(lockId);
+  console.log(attempt);
+  if (attempt.action_attempt.status === 'success') {
+    exec('termux-toast "Door locked"');
+  }
+  process.exit();
+};
+
+if (process.argv[2] === 'unlock') {
+  unlock();
+} else {
+  lock();
+}
+
+
